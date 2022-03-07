56e43bbbb057b1c08f848a0902637958aa0ec3ea
tanulo <tanulo@katonaj-mkszig.hu>
2022-03-07 09:36:48 +0100

    2022.03.07. második feltöltés

---
+++ Rózsahegyi Ákos/Sztringek/string_szeleteles.py
@@ -0,0 +1,7 @@
+s = "A Karib-tenger kalózai"
+print(s[0:1]) # A
+print(s[2:14]) # Karib-tenger
+print(s[15:22]) # kalózai
+
+baratok = ["Misi","Petra","Botond","Jani","Csilla","Peti","Norbi"]
+print(baratok[2:4]) # ["Botond", "Jani"]
