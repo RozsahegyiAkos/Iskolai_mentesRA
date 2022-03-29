5d326683d81d95a83567c33aa05b4a875ff52f99
tanulo <tanulo@katonaj-mkszig.hu>
2022-03-29 14:01:11 +0200

    2022.03.29. Órai munka

--- Rózsahegyi Ákos/PyCharm munka/beolvas1.py
+++ Rózsahegyi Ákos/PyCharm munka/beolvas1.py
@@ -1,8 +1,8 @@
 nyelvek = []
 with open("prognyelvek.csv", "r", encoding="utf-8") as forrasfajl:
     for sor in forrasfajl:
-        adatok = sor.strip().split(",")
-        nyelv = {"evszam": int(adatok[0]), "programnyelv": adatok[1], "nev": adatok[2],  "feltalalo": adatok[3]}
+        adatok = sor.strip().split(";")
+        nyelv = {"evszam": int(adatok[0]), "nyelv": adatok[1], "feltalalo": adatok[2]}
         nyelvek.append(nyelv)
 
 print(f"{nyelvek=}")
