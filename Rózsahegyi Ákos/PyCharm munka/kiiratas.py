2bb9c248e3e3d276c06c7cd213293e5248ba814b
tanulo <tanulo@katonaj-mkszig.hu>
2022-04-04 08:33:01 +0200

    2022.04.04 Órai munka

---
+++ Rózsahegyi Ákos/PyCharm munka/kiiratas.py
@@ -0,0 +1,29 @@
+'''
+Megnyitási módok:
+r: Olvasás
+w: Írás, fájlt hoz létre; ha léztezik ilyen nevű fájl már, felülírja az eredetit
+x: Írás, fájlt hoz létre; ha léztezik ilyen nevű fájl már, hibát ad
+a: A létező fájl végére hozzáfűz, ha még nem létezik ilyen nevű, akkor létrehozza
+a+: Hozzáfűzést és olvasást is lehetővé tesz
+'''
+
+with open('kimenet.txt', 'w', encoding='utf-8') as celfajl:
+    print('Ez kerül a fájlba...', file=celfajl)
+
+with open('kimenet.txt', 'w', encoding='utf-8') as celfajl:
+    celfajl.write('alma, körte, eper')
+    # sztringet ír a fájlba
+
+    # lista elemeit írja a fájlba
+    celfajl.writelines(['alma\n', 'körte\n', 'eper\n'])
+
+
+# Olvasás - írás
+with open('gyumolcsok.txt', 'r', encoding='utf-8') as forrasfajl, \
+    open('gyumolcsok_masolat.txt', 'w', encoding='utf-8') as celfajl:
+    for sor in forrasfajl:
+        print(sor.strip(), file=celfajl)
+
+with open('./adatok/gyumolcsok.txt', 'r', encoding='utf-8') as forrasfajl, \
+    open('./adatok/gyumolcsok_masolat.txt', 'w', encoding='utf-8') as celfajl:
+    celfajl.write(forrasfajl.read())
